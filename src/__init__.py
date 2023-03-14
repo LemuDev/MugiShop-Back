00a01e129c550c1e6a27420c9742d1e776d10028
lemuDev <cruzlemuel0@gmail.com>
2023-03-14 17:15:26 -0400

    Connect DB

--- src/__init__.py
+++ src/__init__.py
@@ -1,3 +1,17 @@
 from flask import Flask
+from src.config.db import db
+
+from decouple import config
+from os import getcwd
+
 
 app = Flask(__name__)
+
+app.config["SQLALCHEMY_DATABASE_URI"] = config('SQLALCHEMY_DATABASE_URI')
+
+
+db.init_app(app)
+
+
+
+
