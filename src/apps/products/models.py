cc6e214c4efbfb358f1ec5e3438c892ce7330362
lemuDev <cruzlemuel0@gmail.com>
2023-03-17 20:34:15 -0400

    Se puso como campo de valor unico name

--- src/apps/products/models.py
+++ src/apps/products/models.py
@@ -6,7 +6,7 @@ from datetime import date
 
 class Products(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String(60))
+    name = db.Column(db.String(60), unique=True)
     description = db.Column(db.String(255))
     price = db.Column(db.Integer())
     image = db.Column(db.String(255))
