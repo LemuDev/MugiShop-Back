159c7e3bf971e4285ccfacbb9874f92b6f5e12d7
lemuDev <cruzlemuel0@gmail.com>
2023-04-03 11:50:31 -0400

    Endpoint de registro realizado

---
+++ src/apps/Auth/models.py
@@ -0,0 +1,13 @@
+from src.config.db import db
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    password = db.Column(db.String(), nullable=False)
+    
+    is_admin = db.Column(db.Boolean, default=False, nullable=False)
