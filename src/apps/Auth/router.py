d5e6c1efa0bca9100c1eafefd8a0f98fa0fef7cb
lemuDev <cruzlemuel0@gmail.com>
2023-04-06 08:41:04 -0400

    Solucion de endpoint de register

--- src/apps/Auth/router.py
+++ src/apps/Auth/router.py
@@ -22,73 +22,72 @@ bp = Blueprint('auth', __name__, url_prefix="/api")
 
 @bp.route("/register", methods=["POST"])
 def register():
-    first_name = request.json.get("first_name")
-    last_name = request.json.get("last_name")
-    email = request.json.get("email")
-    password = request.json.get("password")
-    
-    
-    if first_name == None or last_name == None or email == None or password == None:
-    
+
+    if not request.is_json:
+        
         return jsonify({
             "Error": "No se pasan los datos necesarios para crear un usuario"
         })
-        
-    else:
-        first_name = first_name.strip()
-        last_name = last_name.strip()
-        email = email.strip()
-        password = password.strip()
-        
-
-        
-        new_user_data = {
-            "first_name": first_name,
-            "last_name": last_name,
-            "email": email,
-            "password": password
-        }
-
-        errors = UserValidator().validate(data=new_user_data)
-
-   
-        if len(errors) >= 1:
     
-            return jsonify(errors= errors)
-        
-        else:
-            user_by_email_count = Users.query.filter_by(email=email).count()
+    
+    data = request.json
+    
+    first_name = data.get("first_name")
+    last_name = data.get("last_name")
+    email = data.get("email")
+    password = data.get("password")
+    
+    if first_name == None or last_name == None or email == None or password == None:
+        return jsonify({
+            "Error": "No se pasan los datos necesarios para crear un usuario"
+        })
+    
+    
+    new_user_data = {
+        "first_name": first_name,
+        "last_name": last_name,
+        "email": email,
+        "password": password
+    }
+
+    errors = UserValidator().validate(data=new_user_data)
+
+
+    if len(errors) >= 1:
+
+        return jsonify(errors= errors)
+    
+
+    user_by_email_count = Users.query.filter_by(email=email).count()
+
+    if user_by_email_count >= 1:
+        return jsonify({
+            "errors": {
+                "email": ["Este email ya esta registrado"]
+            }
+        })
+
+
+    password_hash = generate_password_hash(password)
+
+    create_user = Users()
+    create_user.first_name = first_name
+    create_user.last_name = last_name
+    create_user.email = email
+    create_user.password = password_hash
+    create_user.is_admin = False
+    
+    db.session.add(create_user)
+    db.session.commit()
+    
+    if check_password_hash(password=password, pwhash=password):
             
-            if user_by_email_count >= 1:
-                return jsonify({
-                    "errors": {
-                        "email": ["Este email ya esta registrado"]
-                    }
-                })
+            access_token = create_access_token(identity=email, expires_delta=datetime.timedelta(days=20))
+            return jsonify(access_token=access_token)
+
+
     
 
-            password_hash = generate_password_hash(password)
-   
-            create_user = Users()
-            create_user.first_name = first_name
-            create_user.last_name = last_name
-            create_user.email = email
-            create_user.password = password_hash
-            create_user.is_admin = False
-            
-            db.session.add(create_user)
-            db.session.commit()
-                
-                
-            if check_password_hash(password=password, pwhash=password_hash):
-                
-                access_token = create_access_token(identity=email, expires_delta=datetime.timedelta(days=20))
-                return jsonify(access_token=access_token)
-        
-            return jsonify({
-                "message": "Usuario Creado Correctamente"
-            })
-    
 @bp.route("/login", methods=["POST"])
 def login():
     email = request.json.get("email")
--- src/apps/Shop/seed_products.py
+++ src/apps/Shop/seed_products.py
@@ -2,71 +2,194 @@ products = [
     {
         "name": "El rombo 4D",
         "price": 10.99,
-        "image": "images/00012.png",
+        "image": "/Abstract/00012.png",
         "is_sell": False,
         "category": 1
     },
     {
         "name": "El hombre del glitch",
         "price": 10.99,
-        "image": "images/00028.png",
+        "image": "/Abstract/00028.png",
         "is_sell": False,
         "category": 1
     },
     {
         "name": "Fosil Tecnologico",
         "price": 10.99,
-        "image": "images/00038.png",
+        "image": "/Abstract/00038.png",
         "is_sell": False,
         "category": 1
     },
     {
         "name": "Titan Dragon",
         "price": 10.99,
-        "image": "images/00064.png",
+        "image": "/Abstract/00064.png",
         "is_sell": False,
         "category": 1
     },
     {
         "name": "Fungie Blue",
         "price": 10.99,
-        "image": "images/00065.png",
+        "image": "/Abstract/00065.png",
         "is_sell": False,
         "category": 1
     },
     {
         "name": "velociRaptormus Prime",
         "price": 10.99,
-        "image": "images/00097.png",
+        "image": "/Abstract/00097.png",
         "is_sell": False,
         "category": 1
     },
     {
         "name": "Desenfoque por miedo",
         "price": 10.99,
-        "image": "images/00073.png",
+        "image": "/Abstract/00073.png",
         "is_sell": False,
         "category": 1
     },
     {
         "name": "the Iguana-bot",
         "price": 10.99,
-        "image": "images/00095.png",
+        "image": "/Abstract/00095.png",
         "is_sell": False,
         "category": 1
     },
     {
         "name": "the Iguana-bot female",
         "price": 10.99,
-        "image": "images/00096.png",
+        "image": "/Abstract/00096.png",
         "is_sell": False,
         "category": 1
     },
     {
-        "name": "Venezuela",
+        "name": "Socialism Human",
         "price": 10.99,
-        "image": "images/00051.png",
+        "image": "/Abstract/00051.png",
         "is_sell": False,
         "category": 1
+    },
+
+
+    {
+        "name": "La Bestia en Moto",
+        "price": 10.99,
+        "image": "/Animals/00001.png",
+        "is_sell": False,
+        "category": 2
+    },
+    {
+        "name": "La Mirada de la Noche",
+        "price": 10.99,
+        "image": "/Animals/00016.png",
+        "is_sell": False,
+        "category": 2
+    },
+    {
+        "name": "Zorro en Sakura",
+        "price": 10.99,
+        "image": "/Animals/00029.png",
+        "is_sell": False,
+        "category": 2
+    },
+    {
+        "name": "El Pez del Vuelo",
+        "price": 10.99,
+        "image": "/Animals/00039.png",
+        "is_sell": False,
+        "category": 2
+    },
+    {
+        "name": "Cute",
+        "price": 10.99,
+        "image": "/Animals/00071.png",
+        "is_sell": False,
+        "category": 2
+    },
+    {
+        "name": "El perro Tiguere",
+        "price": 10.99,
+        "image": "/Animals/00091.png",
+        "is_sell": False,
+        "category": 2
+    },
+    {
+        "name": "Los perros Tiguere",
+        "price": 10.99,
+        "image": "/Animals/00092.png",
+        "is_sell": False,
+        "category": 2
+    },
+    {
+        "name": "Cyborg CAT",
+        "price": 10.99,
+        "image": "/Animals/00093.png",
+        "is_sell": False,
+        "category": 2
+    },
+    {
+        "name": "Cyborg Zoombie CAT",
+        "price": 10.99,
+        "image": "/Animals/00094.png",
+        "is_sell": False,
+        "category": 2
+    },
+
+
+    {
+        "name": "Defensora de Guerra",
+        "price": 10.99,
+        "image": "/Anime/00023.png",
+        "is_sell": False,
+        "category": 3
+    },
+    {
+        "name": "China Blue",
+        "price": 10.99,
+        "image": "/Anime/00046.png",
+        "is_sell": False,
+        "category": 3
+    },
+    {
+        "name": "El mounstro Domado",
+        "price": 10.99,
+        "image": "/Anime/00047.png",
+        "is_sell": False,
+        "category": 3
+    },
+    {
+        "name": "Aventura de Montaña",
+        "price": 10.99,
+        "image": "/Anime/00048.png",
+        "is_sell": False,
+        "category": 3
+    },
+    {
+        "name": "Realida virtual dentro de la NO-Realidad",
+        "price": 10.99,
+        "image": "/Anime/00049.png",
+        "is_sell": False,
+        "category": 3
+    },
+    {
+        "name": "La flor Samurai",
+        "price": 10.99,
+        "image": "/Anime/00050.png",
+        "is_sell": False,
+        "category": 3
+    },
+    {
+        "name": "Playa Anime",
+        "price": 10.99,
+        "image": "/Anime/00069.png",
+        "is_sell": False,
+        "category": 3
+    },
+    {
+        "name": "Antes de la destruccion",
+        "price": 10.99,
+        "image": "/Anime/00072.png",
+        "is_sell": False,
+        "category": 3
     }
 ];
