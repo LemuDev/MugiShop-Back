c7420f13857ae1c7d4b5bf3d7e73e0304e58aa78
lemuDev <cruzlemuel0@gmail.com>
2023-04-06 21:27:02 -0400

    Seed con todos los productos

--- src/apps/Shop/seed_products.py
+++ src/apps/Shop/seed_products.py
@@ -191,5 +191,578 @@ products = [
         "image": "/Anime/00072.png",
         "is_sell": False,
         "category": 3
-    }
+    },
+    {
+        "name": "Samurai de Fuego",
+        "price": 10.99,
+        "image": "/Anime/00074.png",
+        "is_sell": False,
+        "category": 3
+    },
+    {
+        "name": "School Friends",
+        "price": 10.99,
+        "image": "/Anime/00080.png",
+        "is_sell": False,
+        "category": 3
+    },
+    {
+        "name": "Scorpio Gold",
+        "price": 10.99,
+        "image": "/Anime/00081.png",
+        "is_sell": False,
+        "category": 3
+    },
+    {
+        "name": "CBum le queda corto",
+        "price": 10.99,
+        "image": "/Anime/00082.png",
+        "is_sell": False,
+        "category": 3
+    },
+    {
+        "name": "Guerrero Cinta Roja",
+        "price": 10.99,
+        "image": "/Anime/00083.png",
+        "is_sell": False,
+        "category": 3
+    },
+    {
+        "name": "Anime futurista",
+        "price": 10.99,
+        "image": "/Anime/00098.png",
+        "is_sell": False,
+        "category": 3
+    },
+    
+    
+    
+    {
+        "name": "Mounstro Morado",
+        "price": 10.99,
+        "image": "/Characters/00010.png",
+        "is_sell": False,
+        "category": 4
+    },
+    {
+        "name": "Guerrera de hierro",
+        "price": 10.99,
+        "image": "/Characters/00022.png",
+        "is_sell": False,
+        "category": 4
+    },
+    {
+        "name": "Experiencia Shaolin",
+        "price": 10.99,
+        "image": "/Characters/00025.png",
+        "is_sell": False,
+        "category": 4
+    },
+    {
+        "name": "Peinado Tradicional",
+        "price": 10.99,
+        "image": "/Characters/00030.png",
+        "is_sell": False,
+        "category": 4
+    },
+    {
+        "name": "Reina de Wakanda",
+        "price": 10.99,
+        "image": "/Characters/00077.png",
+        "is_sell": False,
+        "category": 4
+    },
+    {
+        "name": "Reina de Andorra",
+        "price": 10.99,
+        "image": "/Characters/00078.png",
+        "is_sell": False,
+        "category": 4
+    },
+    {
+        "name": "Princesa de Andorra",
+        "price": 10.99,
+        "image": "/Characters/00079.png",
+        "is_sell": False,
+        "category": 4
+    },
+    
+    
+    
+    
+    
+    {
+        "name": "Pitufo verde",
+        "price": 10.99,
+        "image": "/Creatures/00002.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "3 ojos",
+        "price": 10.99,
+        "image": "/Creatures/00003.png",
+        "is_sell": False,
+        "category": 5
+    },       
+    {
+        "name": "Blur Bot",
+        "price": 10.99,
+        "image": "/Creatures/00004.png",
+        "is_sell": False,
+        "category": 5
+    },  
+    {
+        "name": "La caja de la mirada",
+        "price": 10.99,
+        "image": "/Creatures/00005.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "Nube Negra",
+        "price": 10.99,
+        "image": "/Creatures/00006.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "Oso Polar Peluche",
+        "price": 10.99,
+        "image": "/Creatures/00007.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "Coco en evolucion",
+        "price": 10.99,
+        "image": "/Creatures/00008.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "Dinosaurio De dos cabezas",
+        "price": 10.99,
+        "image": "/Creatures/00009.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "Casa Duende",
+        "price": 10.99,
+        "image": "/Creatures/00044.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "Duede de madera",
+        "price": 10.99,
+        "image": "/Creatures/00045.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "Perro Ahorrador",
+        "price": 10.99,
+        "image": "/Creatures/00080.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "Funko Azul",
+        "price": 10.99,
+        "image": "/Creatures/00084.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "The GOAT",
+        "price": 10.99,
+        "image": "/Creatures/00085.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "Casi GOAT",
+        "price": 10.99,
+        "image": "/Creatures/00086.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "La vaquita",
+        "price": 10.99,
+        "image": "/Creatures/00087.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "Perro-Elefante Faraon",
+        "price": 10.99,
+        "image": "/Creatures/00088.png",
+        "is_sell": False,
+        "category": 5
+    },
+    {
+        "name": "Tiburon Jefe",
+        "price": 10.99,
+        "image": "/Creatures/00089.png",
+        "is_sell": False,
+        "category": 5
+    },
+
+
+
+
+    {
+        "name": "Casa Montaña",
+        "price": 10.99,
+        "image": "/Fantasy/00018.png",
+        "is_sell": False,
+        "category": 6
+    },
+    {
+        "name": "Hija de Dracula",
+        "price": 10.99,
+        "image": "/Fantasy/00020.png",
+        "is_sell": False,
+        "category": 6
+    },
+    {
+        "name": "Elfo green",
+        "price": 10.99,
+        "image": "/Fantasy/00031.png",
+        "is_sell": False,
+        "category": 6
+    },
+    {
+        "name": "Piedras flotantes",
+        "price": 10.99,
+        "image": "/Fantasy/00055.png",
+        "is_sell": False,
+        "category": 6
+    },
+    {
+        "name": "Portal 2077",
+        "price": 10.99,
+        "image": "/Fantasy/00056.png",
+        "is_sell": False,
+        "category": 6
+    },
+    {
+        "name": "Portal 2077",
+        "price": 10.99,
+        "image": "/Fantasy/00056.png",
+        "is_sell": False,
+        "category": 6
+    },
+    {
+        "name": "Inicio del terror",
+        "price": 10.99,
+        "image": "/Fantasy/00099.png",
+        "is_sell": False,
+        "category": 6
+    },
+    {
+        "name": "Cara de payaso",
+        "price": 10.99,
+        "image": "/Fantasy/00100.png",
+        "is_sell": False,
+        "category": 6
+    },
+    {
+        "name": "Qué Miras BOBO",
+        "price": 10.99,
+        "image": "/Fantasy/00101.png",
+        "is_sell": False,
+        "category": 6
+    },
+    {
+        "name": "Slenderman-Groot",
+        "price": 10.99,
+        "image": "/Fantasy/00102.png",
+        "is_sell": False,
+        "category": 6
+    },
+    {
+        "name": "Dark Joker",
+        "price": 10.99,
+        "image": "/Fantasy/00103.png",
+        "is_sell": False,
+        "category": 6
+    },
+    
+    
+    
+    
+    {
+        "name": "La Power Ranger Naranja",
+        "price": 10.99,
+        "image": "/Futuristic/00032.png",
+        "is_sell": False,
+        "category": 7
+    },
+    {
+        "name": "Super-Humana",
+        "price": 10.99,
+        "image": "/Futuristic/00033.png",
+        "is_sell": False,
+        "category": 7
+    },
+    {
+        "name": "El Power Ranger Naranja",
+        "price": 10.99,
+        "image": "/Futuristic/00066.png",
+        "is_sell": False,
+        "category": 7
+    },
+    
+    
+    
+    {
+        "name": "Mirada del cerezo",
+        "price": 10.99,
+        "image": "/Landscapes/00013.png",
+        "is_sell": False,
+        "category": 8
+    },
+    {
+        "name": "Montaña colorida",
+        "price": 10.99,
+        "image": "/Landscapes/00021.png",
+        "is_sell": False,
+        "category": 8
+    },
+    {
+        "name": "Modernidad Rural",
+        "price": 10.99,
+        "image": "/Landscapes/00033.png",
+        "is_sell": False,
+        "category": 8
+    },
+    {
+        "name": "Casa Romboide",
+        "price": 10.99,
+        "image": "/Landscapes/00040.png",
+        "is_sell": False,
+        "category": 8
+    },
+    {
+        "name": "Playa Espacial",
+        "price": 10.99,
+        "image": "/Landscapes/00075.png",
+        "is_sell": False,
+        "category": 8
+    },
+    {
+        "name": "Casa en el campo",
+        "price": 10.99,
+        "image": "/Landscapes/00076.png",
+        "is_sell": False,
+        "category": 8
+    },
+
+
+    {
+        "name": "Colonia de hormigas",
+        "price": 10.99,
+        "image": "/Portraits/00000.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "The dinoBots",
+        "price": 10.99,
+        "image": "/Portraits/00011.png",
+        "is_sell": False,
+        "category": 9
+    },    
+    {
+        "name": "Titanic futurista",
+        "price": 10.99,
+        "image": "/Portraits/00014.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Del Oriente",
+        "price": 10.99,
+        "image": "/Portraits/00017.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Ejercicio en la montaña",
+        "price": 10.99,
+        "image": "/Portraits/00019.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Baño de hielo",
+        "price": 10.99,
+        "image": "/Portraits/00026.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "El monte del contraste",
+        "price": 10.99,
+        "image": "/Portraits/00035.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Niebla Oscura",
+        "price": 10.99,
+        "image": "/Portraits/00036.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Noche fria",
+        "price": 10.99,
+        "image": "/Portraits/00037.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Arbol Mounstroso",
+        "price": 10.99,
+        "image": "/Portraits/00041.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Peleador Sakura",
+        "price": 10.99,
+        "image": "/Portraits/00042.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Rascacielos en el monte",
+        "price": 10.99,
+        "image": "/Portraits/00043.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Habitad de dracula",
+        "price": 10.99,
+        "image": "/Portraits/00053.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Rio RTX",
+        "price": 10.99,
+        "image": "/Portraits/00054.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Hija de la Luna",
+        "price": 10.99,
+        "image": "/Portraits/00057.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Europa Nordica",
+        "price": 10.99,
+        "image": "/Portraits/00058.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Hijo de la Luna",
+        "price": 10.99,
+        "image": "/Portraits/00059.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Guerrero Nordico",
+        "price": 10.99,
+        "image": "/Portraits/00060.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "El dios mirando su reinado",
+        "price": 10.99,
+        "image": "/Portraits/00061.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "dios Nordico con hacha",
+        "price": 10.99,
+        "image": "/Portraits/00062.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Diosa Nordica amarilla",
+        "price": 10.99,
+        "image": "/Portraits/00067.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "El padre de los dioses",
+        "price": 10.99,
+        "image": "/Portraits/00068.png",
+        "is_sell": False,
+        "category": 9
+    },
+    {
+        "name": "Vestido liquido",
+        "price": 10.99,
+        "image": "/Portraits/00104.png",
+        "is_sell": False,
+        "category": 9
+    },
+    
+    
+    
+    
+    {
+        "name": "Chica astronauta en el espacio",
+        "price": 10.99,
+        "image": "/Space/00024.png",
+        "is_sell": False,
+        "category": 10
+    },
+    {
+        "name": "Cohetes en el espacio",
+        "price": 10.99,
+        "image": "/Space/00052.png",
+        "is_sell": False,
+        "category": 10
+    },
+    {
+        "name": "Astronauta del futuro",
+        "price": 10.99,
+        "image": "/Space/00070.png",
+        "is_sell": False,
+        "category": 10
+    },
+    
+    
+    
+    
+    
+    
+
+    
+    
+    
+    
+
+
+
+
+
 ];
