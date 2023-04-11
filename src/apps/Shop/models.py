77e58f344401538adf66fc9c7f478f90ba859706
lemuDev <cruzlemuel0@gmail.com>
2023-04-11 14:13:23 -0400

    Definicion de modelo Carrito

--- src/apps/Shop/models.py
+++ src/apps/Shop/models.py
@@ -1,7 +1,6 @@
 from src.config.db import db
 from datetime import date
 
-
 class Categories(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(60), unique=True)
@@ -20,4 +19,14 @@ class Products(db.Model):
     created_at = db.Column(db.Date(), default=date.today() )
     
     categories = db.Column(db.Integer, db.ForeignKey("categories.id"))
+    
+    cart = db.relationship('Cart', backref='Products')
+
+    
+class Cart(db.Model):
+    id = db.Column(db.Integer(), primary_key=True)
+    
+    product_id = db.Column(db.Integer(), db.ForeignKey('products.id'))
+    user_id = db.Column(db.Integer(), db.ForeignKey('users.id'))
+    
     
