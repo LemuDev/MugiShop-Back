ccccf843357df05bf0038a20f52f268d96615db6
lemuDev <cruzlemuel0@gmail.com>
2023-04-15 15:59:50 -0400

    endpoint eliminar producto en carrito

--- src/apps/Shop/router.py
+++ src/apps/Shop/router.py
@@ -5,7 +5,7 @@ from .seed_categories import categories as categories_seed
 from .models import Products, Categories, CartItems, Cart
 from src.apps.Auth.models import Users
 from src.config.db import db
-from .schemas import ProductsSchemas, CategoriesSchemas
+from .schemas import ProductsSchemas, CategoriesSchemas, CartItemSchemas
 
 
 from flask_jwt_extended import jwt_required, get_jwt_identity
@@ -98,8 +98,6 @@ def seed_products():
 def categories_list():
     categories = Categories.query.all()
 
-
-    
     return jsonify(categories_schema.dump(categories))
 
 
@@ -137,9 +135,8 @@ def add_to_cart():
             return jsonify(error="El producto que se intenta agregar no existe"), 404
 
         if product_by_id.is_sell == True:
-            
+           
             return jsonify(error="El producto que se intenta agregar ya fue vendido, las imagenes solo se venden una vez"), 400
-
 
 
         cart_by_user = Cart.query.filter_by(user_id=user_by_email.id).one_or_none()
@@ -157,3 +154,81 @@ def add_to_cart():
         db.session.commit()          
     
         return jsonify(message=f"{product_by_id.name} fue Agregado al carrito") 
+
+@bp.route("/cart", methods=["GET"])
+@jwt_required()
+def cart_list():
+    current_user = get_jwt_identity()
+    user_by_email = Users.query.filter_by(email = current_user).one_or_none()
+    
+    cart_by_user = Cart.query.filter_by(user_id = user_by_email.id).one_or_none()
+    
+    cart_items = CartItems.query.filter_by(cart_id=cart_by_user.id).all()
+    
+    cart = []
+    for c_i in cart_items:
+        
+        product = Products.query.filter_by(id=c_i.product_id).first()
+     
+        cart.append({
+            "id":c_i.id,
+            "product": product.name,
+            "product_img": product.image
+        })
+        
+        
+        print("product_img", c_i.cart.user_id)
+    
+    
+    
+    cart_serializer = CartItemSchemas(many=True)
+    
+    
+    return jsonify(cart_serializer.dump(cart))
+
+
+@bp.route("/cart", methods=["DELETE"])
+@jwt_required()
+def delete_item_cart():
+    current_user = get_jwt_identity()
+    
+    if not request.is_json:
+        
+        return jsonify({
+            "error": "El formato de envio de datos no es correcto"
+        }), 400
+        
+    else:    
+        data = request.json
+        product_item_id = data.get("id")
+        
+        user = Users.query.filter_by(email=current_user).first()
+        
+        try:
+            product_item_id = int(product_item_id)
+        except :    
+            return jsonify(error="El tipo de dato del prodcto debe de ser numerico"), 400 
+        
+        
+        
+        cart = Cart.query.filter_by(user_id=user.id).first()
+
+        product_item = CartItems.query.filter_by(id=product_item_id).filter_by(cart_id=cart.id).one_or_none()
+
+        if product_item == None:
+            return jsonify(error="El producto que se intenta eliminar no esta agregado"), 404
+
+
+
+        db.session.delete(product_item)
+        db.session.commit()
+        
+        product_name = Products.query.filter_by(id=product_item.product_id).first()
+        product_name = product_name.name
+
+
+        return jsonify(message=f"Producto {product_name} eliminado correctamente")
+        
+
+        
+            
